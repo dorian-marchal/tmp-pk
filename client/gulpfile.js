13e1e9ce9917d2b330ce43d40b91973d83c8d100
dorian-marchal <marchal.dorian@gmail.com>
2016-03-28 18:03:46 +0200

    Add client root JS files to linting task

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,7 +3,7 @@ var mocha = require('gulp-mocha');
 var eslint = require('gulp-eslint');
 
 gulp.task('lint', function() {
-    return gulp.src(['./scr/**/*.js', './test/**/*.js'])
+    return gulp.src(['./*.js', './src/**/*.js', './test/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
